3d4b05efb79968fe8cbfd736da7eeb3597c782b6
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-07-02 21:30:21 +0300

    few font changes

--- src/components/Item/ItemFeatures.tsx
+++ src/components/Item/ItemFeatures.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ItemFeatures() {
   return (
-    <div className='flex justify-between my-2.5 md:my-5'>
+    <div className='grid md:flex justify-between my-2.5 md:my-5'>
      <div className=' w-full md:w-1/2'>
      <p className='mediumTitle py-2'>KEY FEATURES</p>
-     <div>
+     <div className='normalText'>
      <p> 1. Style : Casual</p>
      <p> 2. Fabric Type : Satin</p>
       <p>3. Length : Knee Length</p>
@@ -13,7 +13,7 @@ function ItemFeatures() {
       <p> 5. Gender : Women</p>
      </div>
      </div>
-     <div className=' w-full md:w-1/2 grid'>
+     <div className=' w-full md:w-1/2 grid normalText'>
      <p className='mediumTitle py-2'>SPECIFICATIONS</p>
      <div className='flex gap-2'><p>SKU</p> : <p className='text-grey_104'>FA113MW2HDIH2NAFAMZ</p></div>
      <div className='flex gap-2'><p>Production Country</p> : <p className='text-grey_104'>Kenya</p></div>
