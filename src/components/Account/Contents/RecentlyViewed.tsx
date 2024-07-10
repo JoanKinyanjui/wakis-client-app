251a96a7deddf4ec065e5b9b3dd4717896ee3371
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-07-10 17:57:39 +0300

    almost done

--- src/components/Account/Contents/RecentlyViewed.tsx
+++ src/components/Account/Contents/RecentlyViewed.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 function RecentlyViewed() {
   return (
-    <div className='grid grid-cols-2  sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-6  gap-3 w-full  '>
+    <div className='grid grid-cols-2  sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5  gap-3 w-full  '>
     {Products.slice(5,9).map((item)=>(
-      <div key={item.id} className='w-[100px] xxs:w-[120px] md:w-[25%] lg:w-1/5 xl:w-1/6 flex-shrink-0 mx-auto md:mx-0'>
+      <div key={item.id} className='w-full'>
       <ProductImage 
        key={item.id}
        id={item.id}
