e3f1e8b5f75b9a8f022bfb72485e5c509d1f98ac
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2024-07-26 20:05:23 +0800

    fix deployment error

--- src/components/StorePages/StoreProducts.tsx
+++ src/components/StorePages/StoreProducts.tsx
@@ -8,6 +8,7 @@ function StoreProducts() {
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 md:gap-3'>
    {Products.map((item,index)=>(
       <ProductImage 
+      item={item}
       key={item.id}
       id={item.id}
       title= {item.title}
